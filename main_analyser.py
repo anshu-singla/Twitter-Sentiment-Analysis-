6261a77499d4bbdaead2589929bf34cd4069b254
Anshu Singla <anshusingla10121996@gmail.com>
2016-11-18 10:44:10 +0530

    Full word features.
    1. word_features contain full word features for training set

--- main_analyser.py
+++ main_analyser.py
@@ -16,7 +16,10 @@ def get_words_in_tweets(tweets):
    		all_words.extend(words)
    	return all_words
 
-
+def get_word_features(wordlist):
+   	wordlist = nltk.FreqDist(wordlist)
+   	word_features = wordlist.keys()
+   	return word_features
 
 def main():
 	pos_tweets=[]
